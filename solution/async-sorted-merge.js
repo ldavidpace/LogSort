dfb5018bcccabeadc8438e745d9ca4cb3954d7a3
David Pace <ldavidpace@gmail.com>
2024-03-25 22:50:29 -0600

    use Async/Await for cleaner async sort

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,9 +2,7 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-
-
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
   let sources = logSources.map((source) => {
     const promise = source.popAsync();
     const sourceObject = {
@@ -16,36 +14,22 @@ module.exports = (logSources, printer) => {
     });
     return sourceObject;
   });
-
-  const continuePrintingLogs = (orderedSources, resolve) => {
-    const nextLogSource = orderedSources[0];
+  await Promise.all(sources.map(source => source.nextPromise));
+  sources.sort((a,b) => a.next.date.getTime() - b.next.date.getTime() );
+  
+  while(sources.some(source => source.next)) {
+    const nextLogSource = sources[0];
     printer.print(nextLogSource.next);
-    orderedSources = orderedSources.slice(1);
-    nextLogSource.source.popAsync().then((nextMessage) => {
-      nextLogSource.next = nextMessage;
-      if (nextLogSource.next) {
-        const nextIndex = orderedSources.findIndex(source => source.next.date.getTime() > nextLogSource.next.date.getTime());
-        if (nextIndex >= 0) {
-          orderedSources.splice(nextIndex, 0, nextLogSource);
-        } else {
-          orderedSources.push(nextLogSource);
-        }
-      }
-      if (orderedSources.some(source => source.next)) {
-        continuePrintingLogs(orderedSources, resolve);
+    sources = sources.slice(1);
+    nextLogSource.next = await nextLogSource.source.popAsync();
+    if (nextLogSource.next) {
+      const nextIndex = sources.findIndex(source => source.next.date.getTime() > nextLogSource.next.date.getTime());
+      if (nextIndex >= 0) {
+        sources.splice(nextIndex, 0, nextLogSource);
       } else {
-        resolve();
+        sources.push(nextLogSource);
       }
-    });
+    }
   }
-
-  return Promise.all(sources.map(source => source.nextPromise)).then(() => {
-    sources.sort((a,b) => a.next.date.getTime() - b.next.date.getTime() );  
-    return new Promise((resolve, reject) => {
-      continuePrintingLogs(sources, resolve);
-    }).then(() => {
-      printer.done();
-    });
-  });
-
+  printer.done();
 };
